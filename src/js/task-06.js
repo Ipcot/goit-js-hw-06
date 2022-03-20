4d3f50961eef60913335870f44ef9205126a059c
Yuriy Pokhyl <woully@gmail.com>
2022-03-20 22:21:35 +0200

    task 6 done

---
+++ src/js/task-06.js
@@ -0,0 +1,13 @@
+
+const InputData = document.querySelector('#validation-input');
+InputData.addEventListener('input', checkInputValue);
+
+function checkInputValue(event) {
+    let inputSymbol = event.currentTarget.value;
+    InputData.classList.add('invalid');
+    
+    if (inputSymbol.length === Number(InputData.dataset.length)) {
+        InputData.classList.replace('invalid', 'valid');
+    } else InputData.classList.replace('valid', 'invalid');
+    
+}
