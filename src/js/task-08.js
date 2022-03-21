92487d0cf06556a767c65185b9ca3fd65c429c43
Yuriy Pokhyl <woully@gmail.com>
2022-03-21 19:15:47 +0200

    task8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", onFormSubmit)
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const inputEmailEl = event.currentTarget.elements.email.value
+    const inputPasswordEl = event.currentTarget.elements.password.value
+
+    if (inputEmailEl && inputPasswordEl) {
+
+        const formData = {
+            email: inputEmailEl,
+            password: inputPasswordEl,
+        };
+
+        console.log(formData);
+
+        formEl.reset();
+        
+    } else alert("Все поля должны быть заполнены!");
+}
 
