376b28c2091c77b540063883d509b26cb765dcd3
Yuriy Pokhyl <woully@gmail.com>
2022-03-21 22:42:24 +0200

    task9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
+
+const refs = {
+  body: document.querySelector("body"),
+  button: document.querySelector(".change-color"),
+  span: document.querySelector("span"),
+}
+
+refs.button.addEventListener("click", onButtonChangeColor);
+
+function onButtonChangeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
